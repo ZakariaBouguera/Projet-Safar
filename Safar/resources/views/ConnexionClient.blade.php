073088c6a3ed183cc872229b7fc9bbeb77170386
Hakim <hakim.chebab.sio@gmail.com>
2025-03-27 17:27:46 +0100

    Auto stash before merge of "main" and "origin/main"

--- Safar/resources/views/ConnexionClient.blade.php
+++ Safar/resources/views/ConnexionClient.blade.php
@@ -1,13 +1,15 @@
-<form method="POST" action="/clients/connecter" >
-
-{{csrf_field()}}
+<form method="POST" action="/Client/Connecter" >
 
 <label for="nom" > Nom de Connexion : </label>
-<input type="text" name = "login" id="nom">
+<input type="text" name = "email">
+
 <label for ="mdp"> Mot de passe : </label>
-<input type = "password" name = "mdp" id="mdp">
+<input type = "password" name = "mdp">
 
 <input type = "submit" value = "Se connecter">
+
+
+{{csrf_field()}}
 
 </form>
 
